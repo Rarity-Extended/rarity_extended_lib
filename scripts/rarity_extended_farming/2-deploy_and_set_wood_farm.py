c26a6ddf8aa74e5a5838c82710dfff7bede0faf7
Major-Eth <major-tom.eth@pm.me>
2022-02-17 18:13:34 +0100

    ♻️ Style

--- scripts/rarity_extended_farming/2-deploy_and_set_wood_farm.py
+++ scripts/rarity_extended_farming/2-deploy_and_set_wood_farm.py
@@ -5,7 +5,7 @@ from brownie import (
 	rarity_extended_farming_base_premium,
 	Loot
 )
-deployer = accounts.load('rarityextended')
+DEPLOYER = accounts.load('rarityextended')
 
 # Some of the address for Rarity
 RARITY_EXTENDED_MS = '0x0f5861aaf5F010202919C9126149c6B0c76Cf469'
@@ -18,43 +18,43 @@ RARITY_FARMING_CORE = rarity_extended_farming_core.at(RARITY_FARMING_CORE_ADDR)
 def main():
 	# Step 1 - Deploying the Loots contract
 	WOOD_LOOT_0 = Contract.from_explorer(RARITY_EXTENDED_WOOD_LOOT_ADDR)
-	WOOD_LOOT_1 = deployer.deploy(Loot, "Soft Wood", "Soft Wood - (Loot)", publish_source=True, gas_price='250 gwei')
-	WOOD_LOOT_2 = deployer.deploy(Loot, "Fine Wood", "Fine Wood - (Loot)", publish_source=True, gas_price='250 gwei')
-	WOOD_LOOT_3 = deployer.deploy(Loot, "Seasoned Wood", "Seasoned Wood - (Loot)", publish_source=True, gas_price='250 gwei')
-	WOOD_LOOT_4 = deployer.deploy(Loot, "Hard Wood", "Hard Wood - (Loot)", publish_source=True, gas_price='250 gwei')
-	WOOD_LOOT_5 = deployer.deploy(Loot, "Darkwood", "Darkwood - (Loot)", publish_source=True, gas_price='250 gwei')
+	WOOD_LOOT_1 = DEPLOYER.deploy(Loot, "Soft Wood", "Soft Wood - (Loot)", publish_source=True, gas_price='250 gwei')
+	WOOD_LOOT_2 = DEPLOYER.deploy(Loot, "Fine Wood", "Fine Wood - (Loot)", publish_source=True, gas_price='250 gwei')
+	WOOD_LOOT_3 = DEPLOYER.deploy(Loot, "Seasoned Wood", "Seasoned Wood - (Loot)", publish_source=True, gas_price='250 gwei')
+	WOOD_LOOT_4 = DEPLOYER.deploy(Loot, "Hard Wood", "Hard Wood - (Loot)", publish_source=True, gas_price='250 gwei')
+	WOOD_LOOT_5 = DEPLOYER.deploy(Loot, "Darkwood", "Darkwood - (Loot)", publish_source=True, gas_price='250 gwei')
 
 	# Step 2 - Deploying the initial set of farming
-	WOOD_FARMING_0 = deployer.deploy(rarity_extended_farming_base_premium,
+	WOOD_FARMING_0 = DEPLOYER.deploy(rarity_extended_farming_base_premium,
 		RARITY_EXTENDED_MS, 5e18,
 		RARITY_FARMING_CORE, WOOD_LOOT_0, 1, 0, "Rarity Wood",
 		[], [],
 		publish_source=True, gas_price='250 gwei'
 	)
-	WOOD_FARMING_1 = deployer.deploy(rarity_extended_farming_base_premium,
+	WOOD_FARMING_1 = DEPLOYER.deploy(rarity_extended_farming_base_premium,
 		RARITY_EXTENDED_MS, 7e18,
 		RARITY_FARMING_CORE, WOOD_LOOT_1, 1, 1, "Rarity Soft Wood", [WOOD_LOOT_0], [12],
 		publish_source=True, gas_price='250 gwei'
 	)
-	WOOD_FARMING_2 = deployer.deploy(rarity_extended_farming_base_premium,
+	WOOD_FARMING_2 = DEPLOYER.deploy(rarity_extended_farming_base_premium,
 		RARITY_EXTENDED_MS, 11e18,
 		RARITY_FARMING_CORE, WOOD_LOOT_2, 1, 2, "Rarity Fine Wood",
 		[WOOD_LOOT_0, WOOD_LOOT_1], [6, 36],
 		publish_source=True, gas_price='250 gwei'
 	)
-	WOOD_FARMING_3 = deployer.deploy(rarity_extended_farming_base_premium,
+	WOOD_FARMING_3 = DEPLOYER.deploy(rarity_extended_farming_base_premium,
 		RARITY_EXTENDED_MS, 17e18,
 		RARITY_FARMING_CORE, WOOD_LOOT_3, 1, 3, "Rarity Seasoned Wood",
 		[WOOD_LOOT_0, WOOD_LOOT_1, WOOD_LOOT_2], [6, 18, 72],
 		publish_source=True, gas_price='250 gwei'
 	)
-	WOOD_FARMING_4 = deployer.deploy(rarity_extended_farming_base_premium,
+	WOOD_FARMING_4 = DEPLOYER.deploy(rarity_extended_farming_base_premium,
 		RARITY_EXTENDED_MS, 25e18,
 		RARITY_FARMING_CORE, WOOD_LOOT_4, 1, 4, "Rarity Hard Wood",
 		[WOOD_LOOT_0, WOOD_LOOT_1, WOOD_LOOT_2, WOOD_LOOT_3], [6, 18, 36, 120],
 		publish_source=True, gas_price='250 gwei'
 	)
-	WOOD_FARMING_5 = deployer.deploy(rarity_extended_farming_base_premium,
+	WOOD_FARMING_5 = DEPLOYER.deploy(rarity_extended_farming_base_premium,
 		RARITY_EXTENDED_MS, 35e18,
 		RARITY_FARMING_CORE, WOOD_LOOT_5, 1, 5, "Rarity Darkwood",
 		[WOOD_LOOT_0, WOOD_LOOT_1, WOOD_LOOT_2, WOOD_LOOT_3, WOOD_LOOT_4], [6, 18, 36, 60, 180],
@@ -63,19 +63,19 @@ def main():
 
 	# Step 3 - Allowing the farm to mint the loot
 	# WOOD_LOOT_0.setMinter(WOOD_FARMING_0, {"from": RARITY_EXTENDED_OP_MS}) //SKIP, TO DO MANUALY
-	WOOD_LOOT_1.setMinter(WOOD_FARMING_1, {"from": deployer, "gas_price": '250 gwei'})
-	WOOD_LOOT_2.setMinter(WOOD_FARMING_2, {"from": deployer, "gas_price": '250 gwei'})
-	WOOD_LOOT_3.setMinter(WOOD_FARMING_3, {"from": deployer, "gas_price": '250 gwei'})
-	WOOD_LOOT_4.setMinter(WOOD_FARMING_4, {"from": deployer, "gas_price": '250 gwei'})
-	WOOD_LOOT_5.setMinter(WOOD_FARMING_5, {"from": deployer, "gas_price": '250 gwei'})
+	WOOD_LOOT_1.setMinter(WOOD_FARMING_1, {"from": DEPLOYER, "gas_price": '250 gwei'})
+	WOOD_LOOT_2.setMinter(WOOD_FARMING_2, {"from": DEPLOYER, "gas_price": '250 gwei'})
+	WOOD_LOOT_3.setMinter(WOOD_FARMING_3, {"from": DEPLOYER, "gas_price": '250 gwei'})
+	WOOD_LOOT_4.setMinter(WOOD_FARMING_4, {"from": DEPLOYER, "gas_price": '250 gwei'})
+	WOOD_LOOT_5.setMinter(WOOD_FARMING_5, {"from": DEPLOYER, "gas_price": '250 gwei'})
 
 	# Step 4 - Linking the slots in the core contract
-	RARITY_FARMING_CORE.registerFarm(WOOD_FARMING_0, {"from": deployer, "gas_price": '250 gwei'});
-	RARITY_FARMING_CORE.registerFarm(WOOD_FARMING_1, {"from": deployer, "gas_price": '250 gwei'});
-	RARITY_FARMING_CORE.registerFarm(WOOD_FARMING_2, {"from": deployer, "gas_price": '250 gwei'});
-	RARITY_FARMING_CORE.registerFarm(WOOD_FARMING_3, {"from": deployer, "gas_price": '250 gwei'});
-	RARITY_FARMING_CORE.registerFarm(WOOD_FARMING_4, {"from": deployer, "gas_price": '250 gwei'});
-	RARITY_FARMING_CORE.registerFarm(WOOD_FARMING_5, {"from": deployer, "gas_price": '250 gwei'});
+	RARITY_FARMING_CORE.registerFarm(WOOD_FARMING_0, {"from": DEPLOYER, "gas_price": '250 gwei'});
+	RARITY_FARMING_CORE.registerFarm(WOOD_FARMING_1, {"from": DEPLOYER, "gas_price": '250 gwei'});
+	RARITY_FARMING_CORE.registerFarm(WOOD_FARMING_2, {"from": DEPLOYER, "gas_price": '250 gwei'});
+	RARITY_FARMING_CORE.registerFarm(WOOD_FARMING_3, {"from": DEPLOYER, "gas_price": '250 gwei'});
+	RARITY_FARMING_CORE.registerFarm(WOOD_FARMING_4, {"from": DEPLOYER, "gas_price": '250 gwei'});
+	RARITY_FARMING_CORE.registerFarm(WOOD_FARMING_5, {"from": DEPLOYER, "gas_price": '250 gwei'});
 
 	print("=================================================================================")
 	print("RARITY_EXTENDED_WOOD_LOOT_0: '" + Fore.GREEN + WOOD_LOOT_0.address + Style.RESET_ALL + "',")
@@ -91,4 +91,4 @@ def main():
 	print("RARITY_EXTENDED_WOOD_FARM_4: '" + Fore.GREEN + WOOD_FARMING_4.address + Style.RESET_ALL + "',")
 	print("RARITY_EXTENDED_WOOD_FARM_5: '" + Fore.GREEN + WOOD_FARMING_5.address + Style.RESET_ALL + "',")
 	print("=================================================================================")
-	print(deployer.balance().to('ether'))
+	print(DEPLOYER.balance().to('ether'))
--- scripts/rarity_extended_farming/3-deploy_and_set_ore_farm.py
+++ scripts/rarity_extended_farming/3-deploy_and_set_ore_farm.py
@@ -1,11 +1,11 @@
 from colorama import Fore, Style
 from brownie import (
-	accounts, Contract, chain, convert,
+	accounts,
 	rarity_extended_farming_core,
 	rarity_extended_farming_base_premium,
 	Loot
 )
-deployer = accounts.load('rarityextended')
+DEPLOYER = accounts.load('rarityextended')
 
 # Some of the address for Rarity
 RARITY_EXTENDED_MS = '0x0f5861aaf5F010202919C9126149c6B0c76Cf469'
@@ -17,44 +17,44 @@ RARITY_FARMING_CORE = rarity_extended_farming_core.at(RARITY_FARMING_CORE_ADDR)
 
 def main():
 	# Step 1 - Deploying the Loots contract
-	ORE_LOOT_0 = deployer.deploy(Loot, "Copper Ore", "Copper Ore (Loot)", publish_source=True, gas_price='250 gwei')
-	ORE_LOOT_1 = deployer.deploy(Loot, "Iron Ore", "Iron Ore - (Loot)", publish_source=True, gas_price='250 gwei')
-	ORE_LOOT_2 = deployer.deploy(Loot, "Gold Ore", "Gold Ore - (Loot)", publish_source=True, gas_price='250 gwei')
-	ORE_LOOT_3 = deployer.deploy(Loot, "Platinum Ore", "Platinum Ore - (Loot)", publish_source=True, gas_price='250 gwei')
-	ORE_LOOT_4 = deployer.deploy(Loot, "Mithril Ore", "Mithril Ore - (Loot)", publish_source=True, gas_price='250 gwei')
-	ORE_LOOT_5 = deployer.deploy(Loot, "Orichalcum Ore", "Orichalcum Ore - (Loot)", publish_source=True, gas_price='250 gwei')
+	ORE_LOOT_0 = DEPLOYER.deploy(Loot, "Copper Ore", "Copper Ore (Loot)", publish_source=True, gas_price='250 gwei')
+	ORE_LOOT_1 = DEPLOYER.deploy(Loot, "Iron Ore", "Iron Ore - (Loot)", publish_source=True, gas_price='250 gwei')
+	ORE_LOOT_2 = DEPLOYER.deploy(Loot, "Gold Ore", "Gold Ore - (Loot)", publish_source=True, gas_price='250 gwei')
+	ORE_LOOT_3 = DEPLOYER.deploy(Loot, "Platinum Ore", "Platinum Ore - (Loot)", publish_source=True, gas_price='250 gwei')
+	ORE_LOOT_4 = DEPLOYER.deploy(Loot, "Mithril Ore", "Mithril Ore - (Loot)", publish_source=True, gas_price='250 gwei')
+	ORE_LOOT_5 = DEPLOYER.deploy(Loot, "Orichalcum Ore", "Orichalcum Ore - (Loot)", publish_source=True, gas_price='250 gwei')
 
 	# Step 2 - Deploying the initial set of farming
-	ORE_FARMING_0 = deployer.deploy(rarity_extended_farming_base_premium,
+	ORE_FARMING_0 = DEPLOYER.deploy(rarity_extended_farming_base_premium,
 		RARITY_EXTENDED_MS, 5e18,
 		RARITY_FARMING_CORE, ORE_LOOT_0, 1, 0, "Rarity Copper Ore",
 		[], [],
 		publish_source=True, gas_price='250 gwei'
 	)
-	ORE_FARMING_1 = deployer.deploy(rarity_extended_farming_base_premium,
+	ORE_FARMING_1 = DEPLOYER.deploy(rarity_extended_farming_base_premium,
 		RARITY_EXTENDED_MS, 7e18,
 		RARITY_FARMING_CORE, ORE_LOOT_1, 1, 1, "Rarity Iron Ore", [ORE_LOOT_0], [12],
 		publish_source=True, gas_price='250 gwei'
 	)
-	ORE_FARMING_2 = deployer.deploy(rarity_extended_farming_base_premium,
+	ORE_FARMING_2 = DEPLOYER.deploy(rarity_extended_farming_base_premium,
 		RARITY_EXTENDED_MS, 11e18,
 		RARITY_FARMING_CORE, ORE_LOOT_2, 1, 2, "Rarity Gold Ore",
 		[ORE_LOOT_0, ORE_LOOT_1], [6, 36],
 		publish_source=True, gas_price='250 gwei'
 	)
-	ORE_FARMING_3 = deployer.deploy(rarity_extended_farming_base_premium,
+	ORE_FARMING_3 = DEPLOYER.deploy(rarity_extended_farming_base_premium,
 		RARITY_EXTENDED_MS, 17e18,
 		RARITY_FARMING_CORE, ORE_LOOT_3, 1, 3, "Rarity Platinum Ore",
 		[ORE_LOOT_0, ORE_LOOT_1, ORE_LOOT_2], [6, 18, 72],
 		publish_source=True, gas_price='250 gwei'
 	)
-	ORE_FARMING_4 = deployer.deploy(rarity_extended_farming_base_premium,
+	ORE_FARMING_4 = DEPLOYER.deploy(rarity_extended_farming_base_premium,
 		RARITY_EXTENDED_MS, 25e18,
 		RARITY_FARMING_CORE, ORE_LOOT_4, 1, 4, "Rarity Mithril Ore",
 		[ORE_LOOT_0, ORE_LOOT_1, ORE_LOOT_2, ORE_LOOT_3], [6, 18, 36, 120],
 		publish_source=True, gas_price='250 gwei'
 	)
-	ORE_FARMING_5 = deployer.deploy(rarity_extended_farming_base_premium,
+	ORE_FARMING_5 = DEPLOYER.deploy(rarity_extended_farming_base_premium,
 		RARITY_EXTENDED_MS, 35e18,
 		RARITY_FARMING_CORE, ORE_LOOT_5, 1, 5, "Rarity Orichalcum Ore",
 		[ORE_LOOT_0, ORE_LOOT_1, ORE_LOOT_2, ORE_LOOT_3, ORE_LOOT_4], [6, 18, 36, 60, 180],
@@ -62,20 +62,20 @@ def main():
 	)
 
 	# Step 3 - Allowing the farm to mint the loot
-	ORE_LOOT_0.setMinter(ORE_FARMING_0, {"from": deployer, "gas_price": '250 gwei'})
-	ORE_LOOT_1.setMinter(ORE_FARMING_1, {"from": deployer, "gas_price": '250 gwei'})
-	ORE_LOOT_2.setMinter(ORE_FARMING_2, {"from": deployer, "gas_price": '250 gwei'})
-	ORE_LOOT_3.setMinter(ORE_FARMING_3, {"from": deployer, "gas_price": '250 gwei'})
-	ORE_LOOT_4.setMinter(ORE_FARMING_4, {"from": deployer, "gas_price": '250 gwei'})
-	ORE_LOOT_5.setMinter(ORE_FARMING_5, {"from": deployer, "gas_price": '250 gwei'})
+	ORE_LOOT_0.setMinter(ORE_FARMING_0, {"from": DEPLOYER, "gas_price": '250 gwei'})
+	ORE_LOOT_1.setMinter(ORE_FARMING_1, {"from": DEPLOYER, "gas_price": '250 gwei'})
+	ORE_LOOT_2.setMinter(ORE_FARMING_2, {"from": DEPLOYER, "gas_price": '250 gwei'})
+	ORE_LOOT_3.setMinter(ORE_FARMING_3, {"from": DEPLOYER, "gas_price": '250 gwei'})
+	ORE_LOOT_4.setMinter(ORE_FARMING_4, {"from": DEPLOYER, "gas_price": '250 gwei'})
+	ORE_LOOT_5.setMinter(ORE_FARMING_5, {"from": DEPLOYER, "gas_price": '250 gwei'})
 
 	# Step 4 - Linking the slots in the core contract
-	RARITY_FARMING_CORE.registerFarm(ORE_FARMING_0, {"from": deployer, "gas_price": '250 gwei'});
-	RARITY_FARMING_CORE.registerFarm(ORE_FARMING_1, {"from": deployer, "gas_price": '250 gwei'});
-	RARITY_FARMING_CORE.registerFarm(ORE_FARMING_2, {"from": deployer, "gas_price": '250 gwei'});
-	RARITY_FARMING_CORE.registerFarm(ORE_FARMING_3, {"from": deployer, "gas_price": '250 gwei'});
-	RARITY_FARMING_CORE.registerFarm(ORE_FARMING_4, {"from": deployer, "gas_price": '250 gwei'});
-	RARITY_FARMING_CORE.registerFarm(ORE_FARMING_5, {"from": deployer, "gas_price": '250 gwei'});
+	RARITY_FARMING_CORE.registerFarm(ORE_FARMING_0, {"from": DEPLOYER, "gas_price": '250 gwei'});
+	RARITY_FARMING_CORE.registerFarm(ORE_FARMING_1, {"from": DEPLOYER, "gas_price": '250 gwei'});
+	RARITY_FARMING_CORE.registerFarm(ORE_FARMING_2, {"from": DEPLOYER, "gas_price": '250 gwei'});
+	RARITY_FARMING_CORE.registerFarm(ORE_FARMING_3, {"from": DEPLOYER, "gas_price": '250 gwei'});
+	RARITY_FARMING_CORE.registerFarm(ORE_FARMING_4, {"from": DEPLOYER, "gas_price": '250 gwei'});
+	RARITY_FARMING_CORE.registerFarm(ORE_FARMING_5, {"from": DEPLOYER, "gas_price": '250 gwei'});
 
 	print("=================================================================================")
 	print("RARITY_EXTENDED_ORE_LOOT_0: '" + Fore.GREEN + ORE_LOOT_0.address + Style.RESET_ALL + "',")
